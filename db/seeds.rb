c8742103edafd28cba6eba9c0e0c3d8ce42f6855
Johnny Franjo <johnnyfra@mail.com>
2018-10-29 23:05:58 +1100

    Created seed file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+    Blog.create!(
+      title: "My Blog Post number #{blog}",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    )
+
+end
+
+puts "10 Blog Posts created"
+
+5.times do |skill|
+    Skill.create!(
+      title: "My Skill number #{skill}",
+      percent_utilized: 15+skill
+    )
+
+end
+
+puts "5 Skills created"
+
+9.times do |portfolio_item|
+    Portfolio.create!(
+      title: "My Portfolio item #{portfolio_item}}",
+      subtitle: "My Greate Service #{portfolio_item}",
+      body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+      main_image: "https://via.placeholder.com/600x400",
+      thumb_image: "https://via.placeholder.com/300x200"
+    )
+
+end
+
+puts "9 Portfolio items created"
